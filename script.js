395933e2366fe81de22da4401a9b5c698509e972
ldonald1 <lelad12550@gmail.com>
2026-03-30 23:29:30 -0400

    Add buttons and change logic
    Added rock, paper, and scissors buttons that
    select the user's choice when they click on them.
    Changed logic so that scores add up correctly.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+
 
 // generates a random int from 0-2 and 
 // returns either rock paper or scissors 
@@ -21,19 +21,32 @@ function getHumanChoice(){
     return choice;
 }
 
-
-
 // plays five rounds of rock paper scissors, 
 // printing out the final winner after the last round
 function playGame(){
 
-    let humanScore = 0;
-    let computerScore = 0;
+    
 
     // gets human choice and computer choice and
     // compares them to get the winner of that round
-    function playRound(humanChoice, computerChoice){
-        humanChoice = humanChoice.toLowerCase();
+    
+
+
+    // compares final scores to determine
+    // the winner of the game
+    if(humanScore > computerScore){
+        console.log("Human wins with a score of " + humanScore + ":" + computerScore + "!");
+    }
+    else if(computerScore > humanScore){
+        console.log("Computer wins with a score of " + computerScore + ":" + humanScore + "!");
+    }
+    else{
+        console.log("It's a tie! The final score is " + humanScore + ":" + computerScore + "!");
+    }
+
+}
+
+function playRound(humanChoice, computerChoice){
 
         // Case if both have same choice
         if(humanChoice === computerChoice){
@@ -55,30 +68,58 @@ function playGame(){
             console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
             computerScore++;
         }
-    }
 
-
-    // loop that executes playRound 5 times 
-    // to play 5 rounds of rock paper scissors
-    for(let i = 0; i < 5; i++){
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    // compares final scores to determine
-    // the winner of the game
-    if(humanScore > computerScore){
+        if(humanScore === 5){
         console.log("Human wins with a score of " + humanScore + ":" + computerScore + "!");
+        humanScore = 0;
+        computerScore = 0;
     }
-    else if(computerScore > humanScore){
+    else if(computerScore === 5){
         console.log("Computer wins with a score of " + computerScore + ":" + humanScore + "!");
+        humanScore = 0;
+        computerScore = 0;
     }
-    else{
-        console.log("It's a tie! The final score is " + humanScore + ":" + computerScore + "!");
     }
 
-}
+let humanScore = 0;
+let computerScore = 0;
+// initalize humanScore and computerScore
 
-playGame();
+// initialize humanChoice and computerChoice
+let humanChoice = "";
+let computerChoice = "";
+
+const choicesbox = document.querySelector("#choices");
+const results = document.querySelector("#results");
+
+const rock = document.createElement("button");
+rock.textContent = "Rock";
+const paper = document.createElement("button");
+paper.textContent = "Paper";
+const scissors = document.createElement("button");
+scissors.textContent = "Scissors";
+
+choicesbox.appendChild(rock);
+choicesbox.appendChild(paper);
+choicesbox.appendChild(scissors);
+
+
+
+rock.addEventListener("click", () => {
+    humanChoice = "rock";
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
+
+paper.addEventListener("click", () => {
+    humanChoice = "paper";
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
+
+scissors.addEventListener("click", () => {
+    humanChoice = "scissors";
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
 
